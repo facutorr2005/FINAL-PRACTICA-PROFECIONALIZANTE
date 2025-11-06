8579cf03acb90d4ecf266ea6799cd7f5540d3b54
Evelyn3108 <evelynfranco31.08@gmail.com>
2025-11-05 21:22:16 -0300

    Eliminacion pendiente

---
+++ views/eliminarCuenta.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eliminar cuenta</title>
+    <link rel="stylesheet" href="../Public/css/eliminarCuenta.css">
+</head>
+<body>
+    <div class="contenedor">
+        <h2>Confirmar Eliminación</h2>
+        <p>Introduzca su contraseña para confirmar esta acción</p>
+
+        <input type="password" id="contraseña" name="contraseña" placeholder="Introducir contraseña"><br>
+        <input type="password" id="r-contraseña" name="r-contraseña" placeholder="Repetir contraseña"><br>
+        <input type="button" value="Confirmar Eliminación">
+    </div>
+</body>
+</html>
